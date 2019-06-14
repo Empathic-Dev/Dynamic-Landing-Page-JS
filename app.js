6aac8d184ec45c0d020fcac6f97f2a223b9936c6
Deja <theempathicdev@gmail.com>
2019-06-13 23:06:52 -0400

    resized imgs & finished bg/greeting function
    I want to learn how to make the background images responsive

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// dom elements
+  // dom elements
 const time = document.getElementById('time'),
       greeting = document.getElementById('greeting'),
       name = document.getElementById('name'),
       focus = document.getElementById('focus');
 
-// show time
+  // show time
 function showTime(){
   console.log('hello time');
   let today = new Date(),
@@ -12,13 +12,13 @@ function showTime(){
       min = today.getMinutes()
       sec = today.getSeconds();
 
-  // set am or pm
+    // set am or pm
   const amPm = hour >= 12 ? "PM" : "AM";
 
-  // 12hr format
+    // 12hr format
   hour = hour % 12 || 12;
 
-  //output time
+    //output time
   time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${addZero(sec)}`;
 
   setTimeout(showTime, 1000);
@@ -28,5 +28,26 @@ function addZero(n){
   return (parseInt(n, 10) < 10 ? "0" : "") + n;
 }
 
+  //set background & greeting
+function setBgGreet(){
+  let today = new Date(),
+      hour = today.getHours();
+
+  if(hour < 12){
+      // morning
+    document.body.style.backgroundImage = "url('/imgs/morning.jpg')";
+    greeting.textContent = 'Good Morning ';
+  } else if(hour < 18) {
+      // afternoon
+    document.body.style.backgroundImage = "url('/imgs/afternoon.jpg')";
+    greeting.textContent = 'Good Afternoon ';
+  } else {
+      // evening
+    document.body.style.backgroundImage = "url('/imgs/night.jpg')";
+    greeting.textContent = 'Good Night ';
+  }
+}
+
 // run
 showTime();
+setBgGreet();
