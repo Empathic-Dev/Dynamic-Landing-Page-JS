537d0f791ca436ab0b641298f44e634b135735f1
Deja <theempathicdev@gmail.com>
2019-06-13 22:01:12 -0400

    finished html, added styles & finished time function

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("I'm ready. We gone get this bread, queen!");
+// dom elements
+const time = document.getElementById('time'),
+      greeting = document.getElementById('greeting'),
+      name = document.getElementById('name'),
+      focus = document.getElementById('focus');
+
+// show time
+function showTime(){
+  console.log('hello time');
+  let today = new Date(),
+      hour = today.getHours(),
+      min = today.getMinutes()
+      sec = today.getSeconds();
+
+  // set am or pm
+  const amPm = hour >= 12 ? "PM" : "AM";
+
+  // 12hr format
+  hour = hour % 12 || 12;
+
+  //output time
+  time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${addZero(sec)}`;
+
+  setTimeout(showTime, 1000);
+}
+
+function addZero(n){
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+// run
+showTime();
